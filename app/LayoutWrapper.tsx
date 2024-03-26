62f9caa58a6dd680326228e4b97f387e8321e262
asseph <davidkano.dk@gmail.com>
2024-03-26 22:19:39 +0100

    init

---
+++ app/LayoutWrapper.tsx
@@ -0,0 +1,25 @@
+
+import ContextProvider from '@/context/ContextProvider';
+import React from 'react';
+
+/**
+ *
+ * @param Children --> This will be the rendered component in the current page
+ * @returns --> A wrapper of providers such as Session, WalletContext around the Children param
+ */
+type LayoutWrapperProps = {
+  children: React.ReactNode;
+};
+
+const LayoutWrapper: React.FC<LayoutWrapperProps> = async ({ children }) => {
+  return (
+    <>
+      <ContextProvider>
+        {children}
+      </ContextProvider>
+    </>
+
+  );
+};
+
+export default LayoutWrapper;
---
+++ components/shared/MyMultiButton.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import dynamic from 'next/dynamic';
+
+const WalletMultiButtonDynamic = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false }
+);
+
+export default function MyMultiButton() {
+
+  return (
+    <div className=" relative z-[999] bg-black">
+      <WalletMultiButtonDynamic className="mymultibutton" />
+    </div>
+  );
+}
